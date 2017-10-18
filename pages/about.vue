7d720e7ec15c459217adb4613d1be3f1b719ec7c
Lucy20209060 <550502661@qq.com>
2017-10-19 00:07:45 +0800

    nuxt服务器渲染

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      about
+    </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'ABOUT',
+    meta: [
+      { hid: 'description', name: 'description', content: '这是about的描述' }
+    ]
+  }
+}
+</script>
---
+++ pages/mine.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      mine
+    </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'MINE',
+    meta: [
+      { hid: 'description', name: 'description', content: '这是mine的描述' }
+    ]
+  }
+}
+</script>
